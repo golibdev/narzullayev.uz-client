d2cb04dadcde1b1ae299a2ef322ad1a4cc5de833
golibdev <gnarzullayev2000@gmail.com>
2022-12-01 12:34:13 +0500

    Wrapper component created

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Provider from '../contexts/Provider';
 import { MainLayout } from '../layouts/MainLayout';
+import Wrapper from '../layouts/Wrapper';
 import { Blogs } from '../pages/Blogs';
 import { Home } from '../pages/Home';
 import SinglePage from '../pages/SinglePage';
@@ -8,15 +9,17 @@ import SinglePage from '../pages/SinglePage';
 export const App = () => {
    return (
       <Router>
-         <Provider>
-            <Routes>
-               <Route path='/' element={<MainLayout/>}>
-                  <Route index element={<Home/>}/>
-                  <Route path='/blogs' element={<Blogs/>}/>
-                  <Route path='/blogpost/:slug' element={<SinglePage/>}/>
-               </Route>
-            </Routes>
-         </Provider>
+         <Wrapper>
+            <Provider>
+               <Routes>
+                  <Route path='/' element={<MainLayout/>}>
+                     <Route index element={<Home/>}/>
+                     <Route path='/blogs' element={<Blogs/>}/>
+                     <Route path='/blogpost/:slug' element={<SinglePage/>}/>
+                  </Route>
+               </Routes>
+            </Provider>
+         </Wrapper>
       </Router>
    )
 }
